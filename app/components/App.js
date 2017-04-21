9722a83a989693c65c50b0213c97823b97cf4be1
Scott Moschella <scottmosch@gmail.com>
2017-04-21 14:48:57 -0700

    Edit createClass to non-deprecated method

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var createReactClass = require('create-react-class');
 
-var App = React.createClass({
+var App = createReactClass({
   render: function() {
     return (
       <div>Hello World</div>
     )
   }
 });
+
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
