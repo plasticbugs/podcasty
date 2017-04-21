cbc43288ffc9aba5c8002c32d815f0b1ef00ffde
Scott Moschella <scottmosch@gmail.com>
2017-04-21 14:50:00 -0700

    Begin working with routes to pass in url params

--- server-config.js
+++ server-config.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+app.use(express.static('public'));
 
 app.get('/', function(request, response){
-  response.send("woot");
+  response.sendFile(path.resolve(__dirname, './public/index.html'));
 });
+
+app.get('/*', function(request, response){
+  response.redirect('/?channel=' + request.path.slice(1));
+  // response.redirect("http://www." + request.path.slice(1) + ".com");
+})
 
 module.exports = app;
