38be83bb139e8646cce38b1a59291b1c952de969
Scott Moschella <scottmosch@gmail.com>
2017-04-21 17:39:24 -0700

    Add ajax call to get YouTube videos for page

--- app/components/App.js
+++ app/components/App.js
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var createReactClass = require('create-react-class');
 var Router = require('react-router');
+var $ = require('jquery');
 
 // var App = createReactClass({
 //   render: function() {
@@ -10,17 +11,46 @@ var Router = require('react-router');
 //     )
 //   }
 // });
+var lookUpVideos = function (channelID, callback){
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/channels',
+    method: 'GET',
+    data: {
+      key: 'AIzaSyDWPzFJNjsUEfmz5NKoGNP3PHWGrRXxpRk',
+      part: 'contentDetails',
+      forUsername: channelID
+    },
+    success: function(data){
+      $.ajax({
+        url: 'https://www.googleapis.com/youtube/v3/playlistItems',
+        method: 'GET',
+        data: {
+          key: 'AIzaSyDWPzFJNjsUEfmz5NKoGNP3PHWGrRXxpRk',
+          part: 'contentDetails',
+          playlistId: data.items[0].contentDetails.relatedPlaylists.uploads
+        },
+        success: function(data){
+          callback(data);
+        }
+      })
+    }
+  })
+}
 
 class App extends React.Component {
   constructor(props){
     super(props);
-
+    this.state = {
+      videoList: ""
+    }
   }
 
   render() {
     return (
-      <div>{this.props.channel}</div>
-    );
+      <div>{lookUpVideos(this.props.channel, function(data){
+        this.setState({videoList: data})}.bind(this))}
+      </div>
+    )
   }
 }
 
