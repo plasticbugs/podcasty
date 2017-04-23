276e8dd1f3eb94bb85ea4330339b56938109d089
Scott Moschella <scottmosch@gmail.com>
2017-04-23 14:24:42 -0700

    Add conditional to render button if audio is downloaded

--- app/components/VideoEntry.jsx
+++ app/components/VideoEntry.jsx
@@ -1,7 +1,19 @@
 var React = require('react');
+var Percent = require('./Percent.jsx')
+var DownloadButton = require('./DownloadButton.jsx');
 
-var VideoEntry = (props) => (
-  <div>{props.video.snippet.title} -- {props.video.percent}</div>
-);
+function VideoEntry(props) {
+  if(props.video.percent === "100%") {
+    return <div>{props.video.snippet.title}<DownloadButton link={props.video.snippet.resourceId.videoId} channel={props.channel} /></div>
+  } else {
+    return <div>{props.video.snippet.title}<Percent total={props.video.percent} /></div>
+  }
+};
+  // const isLoggedIn = props.isLoggedIn;
+  // if (isLoggedIn) {
+  //   return <UserGreeting />;
+  // }
+  // return <GuestGreeting />;
+
 
 module.exports = VideoEntry;
