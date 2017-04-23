a30164e4f60b1f96db9e05a30d3f40c31c60ba4f
Scott Moschella <scottmosch@gmail.com>
2017-04-23 12:35:18 -0700

    Show percentage downloaded by setting state

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -41,29 +41,32 @@ class App extends React.Component {
     });
 
 //    Every 10 seconds, poll the server and check to see the upload switch
-
+    var that = this;
     setInterval(function(){
       $.ajax({
         url: '/api?channel=' + this.props.channel,
         method: 'GET',
         contentType: 'application/json',
         success: function(data){
-          var data = JSON.parse(data);
-          for(var i = 0; i < this.props.theList; i++){
+          data = JSON.parse(data);
+          for(var i = 0; i < that.state.videoList.length; i++){
             for (var j = 0; j < data.videos.length; j++) {
-              
+              if(that.state.videoList[i].snippet.resourceId.videoId === data.videos[j].id) {
+                that.state.videoList[i].percent = data.videos[j].percent;
+                that.state.videoList[i].done = data.videos[j].done;
+                that.setState({videoList: that.state.videoList});
+              }
             }
           }
           data.videos.forEach(function(video) {
             
           });
-          console.log(JSON.parse(data).videos);
           // videos.forEach(function(video) {
           //   console.log(data);
           // });
         }
       });
-    }.bind(this), 2000);
+    }.bind(this), 1000);
     // setInterval(function(){
     //   var result = []
     //   for(var i = 0; i < this.props.theList.length; i++) {
