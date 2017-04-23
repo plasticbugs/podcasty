d7a890c4330c6e8a714b925c67628930f708d1d9
Scott Moschella <scottmosch@gmail.com>
2017-04-23 14:25:07 -0700

    Add channel to video props for download button

--- app/components/VideoList.jsx
+++ app/components/VideoList.jsx
@@ -4,7 +4,7 @@ var VideoEntry = require('./VideoEntry.jsx');
 var VideoList = (props) => (
   <div className="video-list">
     {props.videos.map(function(video){
-      return <VideoEntry video={video} key={video.snippet.resourceId.videoId} />
+      return <VideoEntry video={video} key={video.snippet.resourceId.videoId} channel={props.channel}/>
     })}
   </div>
 );
