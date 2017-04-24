d479543171dc1225b32a3b428c707b8867cb98ec
Scott Moschella <scottmosch@gmail.com>
2017-04-23 22:37:18 -0700

    Add podcast icon from font awesome

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 var Header = (props) => (
-  <div className="title"><h1>{props.channel}</h1><a className="feed" href={"/feed?uploads=" + props.uploads + "&channel=" + props.channel}>get feed</a></div>
+  <div className="title"><h1>{props.channel}</h1><i className="fa fa-podcast"></i><a className="feed" href={"/feed?uploads=" + props.uploads + "&channel=" + props.channel}>get feed</a></div>
 );
 
 module.exports = Header;
