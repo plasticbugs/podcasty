16568eab8a1fe4e00b50cec1e046a8e090e47ed9
Scott Moschella <scottmosch@gmail.com>
2017-04-23 20:51:28 -0700

    Generate RSS feed for iTunes

--- app/utils/feedGen.js
+++ app/utils/feedGen.js
@@ -3,7 +3,7 @@ var $ = require('jquery');
 
 var generateRSS = function(channelName, uploads){
   var theData = {};
-
+  var feed;
   $.ajax({
     url: 'https://www.googleapis.com/youtube/v3/channels',
     method: 'GET',
@@ -30,19 +30,45 @@ var generateRSS = function(channelName, uploads){
         success: function(data){
           var resultsArray = [];
           for(var i = 0; i < data.items.length; i++) {
-            data.items[i].percent = "0%"
-            data.items[i].done = false;
             resultsArray.push(data.items[i]);
           }
-          console.log(resultsArray);
-          callback(resultsArray);
+          theData.videos = resultsArray;
+          feed = new RSS({
+            title: theData.title,
+            description: theData.description,
+            feed_url: "http://127.0.0.1:3000/feed?uploads=" + uploads + "&channel=" + channelName,
+            site_url: theData.url,
+            image_url: theData.image,
+            custom_namespaces: {
+              'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd'
+            },
+            custom_elements: [
+              {'itunes:summary': theData.description},
+              {'itunes:image': {
+                _attr: { href: theData.image }
+              }}
+            ]
+          });
+          theData.videos.forEach(function(video){
+            var snippet = video.snippet;
+            feed.item({
+              title: snippet.title,
+              description: snippet.description,
+              enclosure: {url:'http://127.0.0.1:3000/', file: 'bitbucket/' + snippet.resourceId.videoId + "__.mp3"},
+              custom_elements: [
+                {'itunes:image': {
+                  _attr: {
+                    href: snippet.thumbnails.medium.url
+                  }
+                }}
+              ]
+            })
+          })
+          return feed.xml();
         }
       })
     }
-    });
-
-    var feed = new RSS(feedOptions);
-
+  });
 }
 
 exports.generateRSS = generateRSS;
