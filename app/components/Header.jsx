10ef53c71892d9db00e5727db40706f00568b0f1
Scott Moschella <scottmosch@gmail.com>
2017-11-06 15:58:29 -0800

    Clean up formatting & styling

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,6 +1,13 @@
 var React = require('react');
 var Header = (props) => (
-  <div className="title"><h1>{props.channel}</h1><a className="feed" href={"/feed?uploads=" + props.uploads + "&channel=" + props.channel}><i className="fa fa-podcast"></i>get feed</a></div>
+  <div className="title">
+    <h1 className="channel-title ">{props.channel}</h1>
+    <a 
+      className="feed" 
+      href={"/feed?uploads=" + props.uploads + "&channel=" + props.channel}>
+        <i className="fa fa-podcast"></i>get feed
+    </a>
+  </div>
 );
 
 module.exports = Header;
--- app/components/VideoEntry.jsx
+++ app/components/VideoEntry.jsx
@@ -4,7 +4,14 @@ var DownloadButton = require('./DownloadButton.jsx');
 
 function VideoEntry(props) {
   if(props.video.percent === "100%") {
-    return <div className="vid-entry"><img src={props.video.snippet.thumbnails.default.url} width='32' height='18'/>{props.video.snippet.title}<DownloadButton link={props.video.snippet.resourceId.videoId} channel={props.channel} /></div>
+    return (<div className="vid-entry">
+        <DownloadButton link={props.video.snippet.resourceId.videoId} channel={props.channel} />
+        <div className="thumb-container">
+          <img src={props.video.snippet.thumbnails.default.url} width="60" height="45"/>
+        </div>
+        {props.video.snippet.title}
+      </div>
+    );
   } else {
     return <div className="vid-entry"><img src={props.video.snippet.thumbnails.default.url} width='32' height='18'/>{props.video.snippet.title}<Percent total={props.video.percent} /></div>
   }
