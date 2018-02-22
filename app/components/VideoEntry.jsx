d7c90aaa70b663007dafed9e197bdbd2ff3d8dac
Scott Moschella <scottmosch@gmail.com>
2018-02-22 12:33:55 -0800

    Refactor and simplify rendering

--- app/components/VideoEntry.jsx
+++ app/components/VideoEntry.jsx
@@ -2,37 +2,19 @@ var React = require('react');
 var Percent = require('./Percent.jsx')
 var DownloadButton = require('./DownloadButton.jsx');
 
-function VideoEntry(props) {
-  if(props.video.percent === "100%") {
-    return (<div className="vid-entry">
-        <DownloadButton link={props.video.snippet.resourceId.videoId} channel={props.channel} />
-        <div className="video-title">
-          {props.video.snippet.title}
-        </div>
-        <div className="thumb-container">
-          <img src={props.video.snippet.thumbnails.default.url} width="60" height="45"/>
-        </div>
+const VideoEntry = (props) => {
+  let downloadOrPercent = props.video.percent === "100%" ? <DownloadButton link={props.video.snippet.resourceId.videoId} channel={props.channel} /> : <Percent total={props.video.percent} />
+  return (
+    <div className="vid-entry">
+      {downloadOrPercent}
+      <div className="video-title">
+        {props.video.snippet.title}
       </div>
-    );
-  } else {
-    return (
-      <div className="vid-entry">
-        <Percent total={props.video.percent} />
-        <div className="video-title">
-          {props.video.snippet.title}
-        </div>
-        <div className="thumb-container">
-          <img src={props.video.snippet.thumbnails.default.url} width='60' height='45'/>
-        </div>
+      <div className="thumb-container">
+        <img src={props.video.snippet.thumbnails.default.url} width="60" height="45"/>
       </div>
-    );
-  }
-};
-  // const isLoggedIn = props.isLoggedIn;
-  // if (isLoggedIn) {
-  //   return <UserGreeting />;
-  // }
-  // return <GuestGreeting />;
-
+    </div>
+  );
+}
 
 module.exports = VideoEntry;
