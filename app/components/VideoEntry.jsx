66c92a9a344fa84966360dbc289389cc8cbccaa2
Scott Moschella <scottmosch@gmail.com>
2018-03-16 15:34:10 -0700

    Refactor list of videos with Semantic UI

--- app/components/VideoEntry.jsx
+++ app/components/VideoEntry.jsx
@@ -1,49 +1,44 @@
-var React = require('react');
-var Percent = require('./Percent.jsx')
-var DownloadButton = require('./DownloadButton.jsx');
+import React, { Component } from 'react';
+import { Item } from 'semantic-ui-react';
 
-const VideoEntry = (props) => {
-  
-  const downloadOrPercent = () => {
-    if (props.video.done) {
-      return (
-        <DownloadButton
-          link={props.video.snippet.resourceId.videoId}
-          channel={props.channel}
-        />
-      )
-    } else if (props.video.percent) {
-      if (props.video.percent === 100) {
-        return (
-          <DownloadButton
-            link={props.video.snippet.resourceId.videoId}
-            channel={props.channel}
-          />
-        )
-      }
-      let total = Math.floor(props.video.percent) + '%';
-      return (<Percent
-        total={total}
-      />);
-    } else {
-      return (
-        <Percent
-          total="...loading"
-        />
-      );
-    }
+import Percent from './Percent.jsx';
+import DownloadButton from './DownloadButton.jsx';
+
+export default class VideoEntry extends Component {
+  constructor(props) {
+    super(props);
   }
-  return (
-    <div className="vid-entry">
-      {downloadOrPercent()}
-      <div className="video-title">
-        {props.video.snippet.title}
-      </div>
-      <div className="thumb-container">
-        <img src={props.video.snippet.thumbnails.default.url} width="60" height="45"/>
-      </div>
-    </div>
-  );
+
+  render() {
+    let ytLink = `http://youtube.com/watch?v=${this.props.video.snippet.resourceId.videoId}`
+    return (
+      <Item>
+        {/* <Item.Image size='small' src={props.video.snippet.thumbnails.default.url} /> */}
+        <Item.Image size='small' src={`https://img.youtube.com/vi/${this.props.video.snippet.resourceId.videoId}/mqdefault.jpg`} />
+  
+        <Item.Content>
+          <Item.Header as='a' href={ytLink}>{this.props.video.snippet.title}</Item.Header>
+          <Item.Meta>Description</Item.Meta>
+          <Item.Description>
+            {this.props.video.snippet.description.slice(0,200).concat('...')}
+          </Item.Description>
+          <Item.Extra>
+          <Percent video={this.props.video} />
+            Published at: {this.props.video.snippet.publishedAt}
+          </Item.Extra>
+        </Item.Content>
+      </Item>
+      // <div className="vid-entry">
+      //   {downloadOrPercent()}
+      //   <div className="video-title">
+      //     {props.video.snippet.title}
+      //   </div>
+      //   <div className="thumb-container">
+      //     <img src={props.video.snippet.thumbnails.default.url} width="60" height="45"/>
+      //   </div>
+      // </div>
+    );
+  }
 }
 
-module.exports = VideoEntry;
+// module.exports = VideoEntry;
