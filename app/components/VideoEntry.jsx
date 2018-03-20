21bf570f3b68e63efac1ff93b2276a5c9ce3f540
Scott Moschella <scottmosch@gmail.com>
2018-03-19 19:41:29 -0700

    SUI Styling

--- app/components/VideoEntry.jsx
+++ app/components/VideoEntry.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Item } from 'semantic-ui-react';
+import timeago from 'timeago.js';
 
 import Percent from './Percent.jsx';
 import DownloadButton from './DownloadButton.jsx';
@@ -13,30 +14,19 @@ export default class VideoEntry extends Component {
     let ytLink = `http://youtube.com/watch?v=${this.props.video.snippet.resourceId.videoId}`
     return (
       <Item>
-        {/* <Item.Image size='small' src={props.video.snippet.thumbnails.default.url} /> */}
         <Item.Image size='small' src={`https://img.youtube.com/vi/${this.props.video.snippet.resourceId.videoId}/mqdefault.jpg`} />
-  
         <Item.Content>
           <Item.Header as='a' href={ytLink}>{this.props.video.snippet.title}</Item.Header>
           <Item.Meta>Description</Item.Meta>
           <Item.Description>
-            {this.props.video.snippet.description.slice(0,200).concat('...')}
+            {this.props.video.snippet.description.slice(0,300).concat('...')}
           </Item.Description>
           <Item.Extra>
           <Percent video={this.props.video} />
-            Published at: {this.props.video.snippet.publishedAt}
+            Published {timeago().format(this.props.video.snippet.publishedAt)}
           </Item.Extra>
         </Item.Content>
       </Item>
-      // <div className="vid-entry">
-      //   {downloadOrPercent()}
-      //   <div className="video-title">
-      //     {props.video.snippet.title}
-      //   </div>
-      //   <div className="thumb-container">
-      //     <img src={props.video.snippet.thumbnails.default.url} width="60" height="45"/>
-      //   </div>
-      // </div>
     );
   }
 }
