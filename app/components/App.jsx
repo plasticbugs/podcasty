b4fa2f8dd2895d748abf1d3ec5585d96061ab1bf
Scott Moschella <scottmosch@gmail.com>
2018-03-19 19:39:40 -0700

    Add SUI css import

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css';
 
+import "semantic-ui-css/semantic.min.css"
 import MainLayout from './MainLayout.jsx';
 
 class App extends React.Component {
